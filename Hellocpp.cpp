334e76a209a1aead1fcfcc83a231cee1486e62bf
jackywoo <wuchienchao@qq.com>
2020-10-18 19:57:45 +0800

    add hello world

---
+++ Hellocpp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wuchi on 2020/10/17.
+//
+
+#include "Hellocpp.h"
---
+++ Hellocpp.h
@@ -0,0 +1,16 @@
+//
+// Created by wuchi on 2020/10/17.
+//
+
+#ifndef LERN_CPP_HELLOCPP_H
+#define LERN_CPP_HELLOCPP_H
+
+
+
+class Hellocpp {
+
+};
+
+
+
+#endif //LERN_CPP_HELLOCPP_H
